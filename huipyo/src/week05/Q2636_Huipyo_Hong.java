62ab43cccffd14b5b1698d28f5d93ae7cab75c85
= <=>
2024-02-16 01:09:13 +0900

    Q2636 치즈 제출 완료

--- huipyo/src/week05/Q2636_Huipyo_Hong.java
+++ huipyo/src/week05/Q2636_Huipyo_Hong.java
@@ -1,3 +1,14 @@
+/**
+ 치즈
+ https://www.acmicpc.net/problem/2636
+ */
+
+/**
+ *  [ 효율성 ]
+ *  - 메모리: 39456KB
+ *  - 시간 : 756ms
+ */
+
 package week05;
 
 import java.util.*;
@@ -8,55 +19,51 @@ public class Q2636_Huipyo_Hong {
         Scanner scanner = new Scanner(System.in);
         int height = scanner.nextInt(), width = scanner.nextInt();
         int[][] grid = IntStream.range(0, height).mapToObj(operand -> IntStream.range(0, width).map(operand1 -> scanner.nextInt()).toArray()).toArray(int[][]::new);
+        int time = 0;
+        int lastCheese = 0;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    Set<int[]> outsides = new HashSet<>();
+        while (true) {
+            int cheese = countCheese(grid);
 
-                    bfs(grid, new int[] {i, j, 0}, outsides);
-                    /*outsides.forEach(ints -> {
-                        grid[ints[0]][ints[1]] = 0;
-                    });*/
-                }
+            if (cheese == 0) {
+                System.out.println(time);
+                System.out.println(lastCheese);
+                break;
             }
-        }
-        for (int[] a : grid) {
-            System.out.println(Arrays.toString(a));
+            if (lastCheese != cheese) lastCheese = cheese;
+            time++;
+            bfs(grid, new AbstractMap.SimpleEntry<>(0, 0));
         }
         scanner.close();
     }
 
-    private static void bfs(int[][] grid, int[] start, Set<int[]> outsides) {
-        Queue<int[]> queue = new ArrayDeque<>(List.of(start));
-        grid[start[0]][start[1]] = mark(grid, start, outsides);
+    private static void bfs(int[][] grid, AbstractMap.SimpleEntry<Integer, Integer> start) {
+        Queue<AbstractMap.SimpleEntry<Integer, Integer>> queue = new ArrayDeque<>(List.of(start));
+        Set<AbstractMap.SimpleEntry<Integer, Integer>> visited = new HashSet<>(List.of(start));
 
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+            AbstractMap.SimpleEntry<Integer, Integer> current = queue.poll();
+            int x = current.getKey();
+            int y = current.getValue();
 
-            for (int[] next : new int[][] {{current[0] - 1, current[1]}, {current[0], current[1] + 1}, {current[0] + 1, current[1]}, {current[0], current[1] - 1}}) {
-                if (next[0] < 0 || next[1] < 0 || next[0] >= grid.length || next[1] >= grid[0].length) continue;
-                if (grid[next[0]][next[1]] == 1) {
-                    grid[next[0]][next[1]] = mark(grid, next, outsides);
+            for (int[] next : new int[][] {{x - 1, y}, {x, y + 1}, {x + 1, y}, {x, y - 1}}) {
+                AbstractMap.SimpleEntry<Integer, Integer> key = new AbstractMap.SimpleEntry<>(next[0], next[1]);
 
-                    queue.offer(new int[] {next[0], next[1], current[2] + 1});
+                if (next[0] >= 0 && next[1] >= 0 && next[0] < grid.length && next[1] < grid[0].length && !visited.contains(key)) {
+                    if (grid[next[0]][next[1]] == 1) {
+                        grid[next[0]][next[1]] = 0;
+                    } else if (grid[next[0]][next[1]] == 0) {
+                        queue.offer(key);
+                    }
+                    visited.add(key);
                 }
             }
         }
     }
 
-    private static boolean isOutside(int[][] grid, int[] next) {
-        for (int[] next1 : new int[][] {{next[0] - 1, next[1]}, {next[0], next[1] + 1}, {next[0] + 1, next[1]}, {next[0], next[1] - 1}}) {
-            if (next1[0] < 0 || next1[1] < 0 || next1[0] >= grid.length || next1[1] >= grid[0].length) continue;
-            if (grid[next1[0]][next1[1]] == 0) return true;
-        }
-        return false;
-    }
-
-    private static int mark(int[][] grid, int[] pos, Set<int[]> outsides) {
-        if (isOutside(grid, pos)) {
-            outsides.add(pos);
-            return 3;
-        } else return 2;
+    private static int countCheese(int[][] grid) {
+        return (int) Arrays.stream(grid)
+                .flatMapToInt(ints -> Arrays.stream(ints).filter(value -> value == 1))
+                .count();
     }
 }
